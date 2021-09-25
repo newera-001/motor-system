2a57122f608b00162c2e1cb251c1047b047bd9f5
NewEra <55731265+newera-001@users.noreply.github.com>
2021-09-25 13:36:21 +0800

    Add files via upload

---
+++ motion_imitation/learning/encoder.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+class Encoder(nn.Module):
+    def __init__(self,params_size,z_size):
+        super(Encoder, self).__init__()
+
+        self.fc = nn.Sequential(
+            nn.Linear(params_size,128),
+            nn.ReLU(),
+            nn.Linear(128,z_size)
+        )
+
+    def forward(self,params):
+        return self.fc(params)
+
+    def optimizer(self, lr=1e-3, parameters = None):
+        return torch.optim.Adam(lr=lr,params=parameters)
